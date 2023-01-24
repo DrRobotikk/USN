17e7d3bf6ab2601c707b934f383118dab333450c
Monosakkarider <97236041+Monosakkarider@users.noreply.github.com>
2023-01-24 13:08:36 +0100

    web

---
+++ Webutvikling/09112021/first_jS.js
@@ -0,0 +1,9 @@
+let x=-9
+let y=3
+let z=x*y
+
+var 1-2="Value is +50"
+
+//console.log(1-2);
+
+document.write(1-2)
---
+++ Webutvikling/Oblig2-web/assignment-2/Tempus.js
@@ -0,0 +1,7 @@
+    var i = 0;
+    function buttonaddClick() {
+        document.getElementById('add').value = ++i;
+    }
+    function buttonsubClick() {
+        document.getElementById('add').value = --i;
+    }
