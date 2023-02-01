f00206e82de8496cb6354629932bd81789cb4c2b
Monosakkarider <97236041+Monosakkarider@users.noreply.github.com>
2023-02-01 14:25:22 +0100

    ja

--- intelliJ4Semester/DemoLagringAvFiler/src/LagreDemo/Kontroll.java
+++ intelliJ4Semester/DemoLagringAvFiler/src/LagreDemo/Kontroll.java
@@ -2,8 +2,10 @@ package LagreDemo;
 
 import Hjelpeklasser.Filbehandling;
 
+import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Kontroll {
     //Datastruktur:
@@ -24,6 +26,41 @@ public class Kontroll {
                 utfil.println(p.toFile());
             }
             utfil.close();
+        } catch (Exception e) {
+        }
+    }
+
+    public ArrayList<Person> getPersoner() {
+        return personer;
+    }
+
+    public void empty() {
+        personer.clear();
+    }
+
+    //Metode for å lese filen og gjennopprettet objektet:
+    public void lesData(String filnavn) {
+        //Tømmer personer-lista:
+        empty();
+        //Innlesing:
+        try {
+            BufferedReader innfil = Filbehandling.lagLeseforbindelse(filnavn);
+            //Leser inn en linje:
+            String linje = innfil.readLine();
+            //Starter en løkke:
+            while (linje != null) {
+                //Opprette et StringTokenizer-objekt:
+                StringTokenizer innhold = new StringTokenizer(linje, ";");
+                //Leser ut attributtverdiene:
+                String navn = innhold.nextToken();
+                String adresse = innhold.nextToken();
+                int fodselsar = Integer.parseInt(innhold.nextToken());
+                //Lager et personobjekt og legger inn i Arraylisten:
+                personer.add(new Person(navn, adresse, fodselsar));
+                //Leser så neste linje i fila:
+                linje = innfil.readLine();
+            }//Ferdig med løkka
+            innfil.close();
         } catch (Exception e) {
         }
     }
--- intelliJ4Semester/DemoLagringAvFiler/src/LagreDemo/Testklient.java
+++ intelliJ4Semester/DemoLagringAvFiler/src/LagreDemo/Testklient.java
@@ -1,5 +1,7 @@
 package LagreDemo;
 
+import java.util.ArrayList;
+
 public class Testklient {
     public static void main(String[] args) {
         String filnavn = "personer.csv";
@@ -7,6 +9,33 @@ public class Testklient {
         //Legger til personer:
         kontroll.nyPerson(new Person("Ole", "Gate 1", 2001));
         kontroll.nyPerson(new Person("Lise", "Gate 2", 2011));
+
+        //Sjekker innholdet:
+        System.out.println("Innholdet i datastrukturen:");
+        ArrayList<Person> personer = kontroll.getPersoner();
+        for (Person p : personer) {
+            if (p != null) System.out.println(p.toString());
+        }
+        //Lagrer:
         kontroll.skrivData(filnavn);
+        //Tømmer Arraylisten i kontroll:
+        kontroll.empty();
+        //Sjekker at den er tom:
+        personer = kontroll.getPersoner();
+        System.out.println("");
+        System.out.println("Personer i datastrukturen etter tømming:");
+        for (Person p : personer) {
+            if (p != null) System.out.println(p.toString());
+        }
+        //Leser fra fil:
+        kontroll.lesData(filnavn);
+        System.out.println("");
+        System.out.println("Sjekker at objektene er gjenopprettet:");
+        personer = kontroll.getPersoner();
+        System.out.println("");
+        System.out.println("Personer i datastrukturen etter gjenoppretting:");
+        for (Person p : personer) {
+            if (p != null) System.out.println(p.toString());
+        }
     }
 }
